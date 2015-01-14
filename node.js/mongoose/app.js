e27615eca966c5e812720881e3e3a4c797dd5777
Suissa <jnascimento@gmail.com>
2015-01-14 20:57:14 -0200

    Encapsulando as funções de Controller em um obj

--- node.js/mongoose/app.js
+++ node.js/mongoose/app.js
@@ -3,6 +3,83 @@ var http = require('http')
   ;
 
 
+var Controller = {
+  create: function(req, res) {
+    var dados = { name: 'Skol'
+      , description: 'Mijo de rato'
+      , alcohol: 4.5
+      , price: 3.0
+      , category: 'pilsen'
+    }
+    , model = new Beer(dados);
+
+    model.save(function (err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        res.end('Erro: ' + err);
+      }
+      else{
+        console.log('Sucesso: ', data);
+        res.end('Sucesso: ' + data);
+      }
+    });
+  },
+  retrieve: function(req, res) {
+    var query = {};
+
+    Beer.find(query, function (err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        res.end('Erro: ' + err);
+      }
+      else{
+        console.log('Sucesso: ', data);
+        res.end('Sucesso: ' + data);
+      }
+    });
+  },
+  update: function(req, res) {
+    var query = {name: /skol/i};
+
+    var mod = {
+      name: 'Brahma',
+      alcohol: 4,
+      price: 6,
+      category: 'pilsen'
+    };
+
+    var optional = {
+      upsert: false,
+      multi: false
+    };
+
+    Beer.update(query, mod, function (err, data) {
+      if (err){
+        console.log('Erro: ', err);
+      }
+      else{
+        console.log('Cervejas atualizadas com sucesso: ', data);
+      }
+      process.exit(0);
+    });
+
+  },
+  delete: function(req, res) {
+    var query = {};
+
+    Beer.remove(query, function(err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        res.end('Erro: ' + err);
+      }
+      else{
+        console.log('Sucesso: ', data);
+        res.end('Sucesso: ' + data);
+      }
+    });
+  }
+};
+
 http.createServer(function(req, res) {
   res.writeHead(200, {"Content-Type": "text/plain"});
 
@@ -11,73 +88,16 @@ http.createServer(function(req, res) {
 
   switch(url) {
     case '/beers/create':
-      var dados = { name: 'Skol'
-        , description: 'Mijo de rato'
-        , alcohol: 4.5
-        , price: 3.0
-        , category: 'pilsen'
-      }
-      , model = new Beer(dados);
-
-      model.save(function (err, data) {
-        if (err){
-          console.log('Erro: ', err);
-          res.end('Erro: ' + err);
-        }
-        else{
-          console.log('Sucesso: ', data);
-          res.end('Sucesso: ' + data);
-        }
-      });
+      Controller.create(req, res);
       break;
     case '/beers/retrieve':
-      var query = {};
-
-      Beer.find(query, function (err, data) {
-        if (err){
-          console.log('Erro: ', err);
-          res.end('Erro: ' + err);
-        }
-        else{
-          console.log('Sucesso: ', data);
-          res.end('Sucesso: ' + data);
-        }
-      });
+      Controller.retrieve(req, res);
       break;
     case '/beers/update':
-      var query = {}
-        , mod = { name: 'Brahma'
-          , alcohol: 4
-          , price: 6
-          , category: 'pilsen'
-        };
-
-      Beer.update(query, mod, function (err, data) {
-        if (err){
-          console.log('Erro: ', err);
-          res.end('Erro: ' + err);
-        }
-        else{
-          console.log('Sucesso: ', data);
-          res.end('Sucesso: ' + data);
-        }
-      });
-
+      Controller.update(req, res);
       break;
     case '/beers/delete':
-      var query = {};
-
-      Beer.remove(query, function(err, data) {
-        if (err){
-          console.log('Erro: ', err);
-          res.end('Erro: ' + err);
-        }
-        else{
-          console.log('Sucesso: ', data);
-          res.end('Sucesso: ' + data);
-        }
-      });
-
+      Controller.delete(req, res);
       break;
   }
 
