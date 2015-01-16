404cd9e0b74d9dd9c1d0d3829a22fd032bb43bec
Suissa <jnascimento@gmail.com>
2015-01-16 20:06:12 -0200

    Subindo inicio do projeto final

---
+++ projeto-final/controllers/beers.js
@@ -0,0 +1,87 @@
+var Model = require('./../models/beer');
+
+
+module.exports = {
+  create: function(req, res) {
+    var dados = req.body
+    , model = new Model(dados);
+
+    console.log('POST: ', dados);
+
+    model.save(function (err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        res.json(err);
+      }
+      else{
+        console.log('Sucesso: ', data);
+        res.json(data);
+      }
+    });
+  },
+  retrieve: function(req, res) {
+    var query = {};
+
+    Model.find(query, function (err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        res.json(err);
+      }
+      else{
+        console.log('Sucesso: ', data);
+        res.json(data);
+      }
+    });
+  },
+  findOne: function(req, res) {
+    var query = {_id: req.params.id};
+
+    Model.findOne(query, function (err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        res.json(err);
+      }
+      else{
+        console.log('Sucesso: ', data);
+        res.json(data);
+      }
+    });
+  },
+  update: function(req, res) {
+    // req.params contém as variáveis que vc setou na rota
+    var query = {_id: req.params.id};
+
+    var mod = req.body;
+
+    var optional = {
+      upsert: false,
+      multi: true
+    };
+
+    Model.update(query, mod, function (err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        res.json(err);
+      }
+      else{
+        console.log('Sucesso: ', data);
+        res.json(data);
+      }
+    });
+
+  },
+  delete: function(req, res) {
+    var query = {_id: req.params.id};
+
+    Model.remove(query, function(err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        res.json(err);
+      }
+      else{
+        console.log('Sucesso: ', data);
+        res.json(data);
+      }
+    });
+  }
+};
---
+++ projeto-final/routes/api/beers.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var Controller = require('./../../controllers/beers')
+
+
+
+// CREATE
+router.post('/', function(req, res) {
+  Controller.create(req, res);
+});
+
+// RETRIEVE
+router.get('/', function(req, res) {
+  Controller.retrieve(req, res);
+});
+
+router.get('/:id', function(req, res) {
+  Controller.findOne(req, res);
+});
+
+// UPDATE
+router.put('/:id', function(req, res) {
+  Controller.update(req, res);
+});
+
+// DELETE
+router.delete('/:id', function(req, res) {
+  Controller.delete(req, res);
+});
+
+module.exports = router;
+
+/*
+Em uma API Rest para o CRUD iremos utilizar 4 verbos do HTTP que são:
+
+- Create = POST
+- Retrieve = GET
+- Update = PUT
+- Delete = delete
+*/
